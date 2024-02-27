hopez13/art

JIT: Fix creating perf map and jit dump files
Make sure the directory kLogPrefix exists before
creating those files

This commit fixes 913-heaps ART test

Test: ./art/test/run-test --target --jit-on-first-use --64 913-heaps
Change-Id: Ia0f5e6bb06509a107e26cf06eaafa4138eaa30c1

compiler/jit/jit_logger.cc
@@ -33,6 +33,12 @@ static const char* kLogPrefix = "/data/misc/trace";
 static const char* kLogPrefix = "/tmp";
 #endif
 
+void JitLogger::EnsurePerfDirictory() {
+  if (!OS::CreateDirectory(kLogPrefix, 0755)) {
+    LOG(WARNING) << "Could not create " << kLogPrefix << " directory";
+  }
+}
+
 // File format of perf-PID.map:
 // +---------------------+
 // |ADDR SIZE symbolname1|

ApexOS/android_art

compiler/jit/jit_logger.cc
@@ -33,6 +33,12 @@ static const char* kLogPrefix = "/data/misc/trace";
 static const char* kLogPrefix = "/tmp";
 #endif
 
+void JitLogger::EnsurePerfDirectory() {
+  if (!OS::CreateDirectory(kLogPrefix, 0755)) {
+    LOG(WARNING) << "Could not create " << kLogPrefix << " directory";
+  }
+}
+
 // File format of perf-PID.map:
 // +---------------------+
 // |ADDR SIZE symbolname1|